196b322d8b6f97df46a9073b0994a2b858cbc8c0
Momo Khan <mkhanongm@gmail.com>
2025-04-09 16:06:40 -0400

    Add our TCA feature to the entry point of the app

--- meet-tca/meet_tcaApp.swift
+++ meet-tca/meet_tcaApp.swift
@@ -5,13 +5,19 @@
 //  Created by Momo Khan on 4/9/25.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct meet_tcaApp: App {
+    static let store = Store(initialState: CounterFeature.State()) { // stores should be created only once, usually at the root of the app
+        CounterFeature()
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView( // alter the entry point of our app to create a view that uses our reducer
+                store: meet_tcaApp.store
+            )
         }
     }
 }
