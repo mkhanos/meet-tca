13fc3a7d34b4fdc6d743c22f16f1f3b11b29e067
Momo Khan <mkhanongm@gmail.com>
2025-04-09 15:26:42 -0400

    Initial Commit

---
+++ meet-tca/meet_tcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  meet_tcaApp.swift
+//  meet-tca
+//
+//  Created by Momo Khan on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct meet_tcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
