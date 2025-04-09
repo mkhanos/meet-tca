011c7b0420a542747c6e574d9f90700137b1d630
Momo Khan <mkhanongm@gmail.com>
2025-04-09 15:38:06 -0400

    Add counter feature

---
+++ meet-tca/CounterFeature.swift
@@ -0,0 +1,11 @@
+//
+//  CounterFeature.swift
+//  meet-tca
+//
+//  Created by Momo Khan on 4/9/25.
+//
+
+import ComposableArchitecture
+
+// Features are built using the Reducer() macro and Reducer protocol
+// They house business logic without any mention of a view
