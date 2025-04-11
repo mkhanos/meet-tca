90fde8a8a969871846d7946623919bb06e8f364d
Momo Khan <mkhanongm@gmail.com>
2025-04-11 10:39:53 -0400

    Add a fact button and async get a fact whcih our app will respond to

--- meet-tca/CounterFeature.swift
+++ meet-tca/CounterFeature.swift
@@ -17,11 +17,15 @@ struct CounterFeature {
     @ObservableState
     struct State { // holds state
         var count = 0
+        var isLoading = false
+        var fact: String?
     }
     
     enum Action { // all the actions a user can perform in the feature
         case incrementButtonTapped // described as the UI actions rather than what the business logic is meant to do
         case decrementButtonTapped
+        case factButtonTapped
+        case factResponse(String) // our reducer responding to the fact being loaded
     }
     
     // typically we compose reducers together to form complex business logic
@@ -31,10 +35,25 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none // return value for the Effect to the outside world
-                
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                // we cannot do asyncrhonous work in the reducer, only state mutations
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
             }
         }
@@ -50,11 +69,21 @@ struct CounterView: View {
     var body: some View {
         VStack {
             Text("\(store.count)") // access state via member look up
-            Button("-") {
-                store.send(.decrementButtonTapped) // send actions via the send method
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped) // send actions via the send method
+                }
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                Button("Fact") {
+                    store.send(.factButtonTapped)
+                }
             }
-            Button("+") {
-                store.send(.incrementButtonTapped)
+            if store.isLoading {
+                ProgressView()
+            } else if let fact = store.fact {
+                Text(fact)
             }
         }
     }
@@ -62,6 +91,6 @@ struct CounterView: View {
 
 #Preview {
     CounterView(store: .init(initialState: CounterFeature.State()) { // provide an initial state for the store as well as the reducer that powers the feature
-        CounterFeature() // we can comment out this reducer 
+        CounterFeature() // we can comment out this reducer
     })
 }
