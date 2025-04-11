70737b4eb1eb6ccaf2636f2e6fb0cb96a67c3a14
Momo Khan <mkhanongm@gmail.com>
2025-04-11 15:33:06 -0400

    Add proper assertions for our increment and decrement tests

--- meet-tcaTests/CounterFeatureTests.swift
+++ meet-tcaTests/CounterFeatureTests.swift
@@ -14,11 +14,15 @@ import Testing
 struct CounferFeatureTests {
     @Test
     func basics() async {
-        let store = TestStore(initialState: CounterFeature.State()) {
+        let store = TestStore(initialState: CounterFeature.State()) { // create a test store the same way a regular store
             CounterFeature()
         }
         
-        await store.send(.incrementButtonTapped)
-        await store.send(.decrementButtonTapped)
+        await store.send(.incrementButtonTapped) { // send actions
+            $0.count = 1 // our responsibility to change the state to what it will be after the event
+        }
+        await store.send(.decrementButtonTapped) {
+            $0.count = 0
+        }
     }
 }
