3124a732d3cf0bb7ea88cf53d32c3090ef6e779a
Momo Khan <mkhanongm@gmail.com>
2025-04-14 13:24:03 -0400

    Add add contact feature

---
+++ meet-tca/AddContactFeature.swift
@@ -0,0 +1,38 @@
+//
+//  AddContactFeature.swift
+//  meet-tca
+//
+//  Created by Momo Khan on 4/14/25.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct AddContactFeature {
+    @ObservableState
+    struct State: Equatable {
+        var contact: Contact
+    }
+    
+    enum Action {
+        case cancelButtonTapped
+        case saveButtonTapped
+        case setName(String)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .cancelButtonTapped:
+                return .none
+            case .saveButtonTapped:
+                return .none
+            case let .setName(name):
+                state.contact.name = name
+                return .none
+            }
+        }
+    }
+}
+
