1e5f32182b9bc6f8c3b57bb839b201f405b5183d
Momo Khan <mkhanongm@gmail.com>
2025-04-14 12:42:04 -0400

    Add app feature composed of two counter feature reducers

---
+++ meet-tca/AppFeature.swift
@@ -0,0 +1,58 @@
+//
+//  AppFeature.swift
+//  meet-tca
+//
+//  Created by Momo Khan on 4/14/25.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct AppFeature {
+    @ObservableState
+    struct State: Equatable {
+        var tab1 = CounterFeature.State()
+        var tab2 = CounterFeature.State()
+    }
+    
+    enum Action {
+        case tab1(CounterFeature.Action)
+        case tab2(CounterFeature.Action)
+    }
+    
+    // we still use the reduce closure for the core business logic but we don't want to repeat the code from CounterFeature
+    var body: some ReducerOf<Self> {
+        // Scope reducer allows us to focus on a sub domain of the parent feature and run a reducer on that subdomain
+        // We use a scope reducer for each tab, and they each use the same reducer
+        Scope(state: \.tab1, action: \.tab1) {
+            CounterFeature()
+        }
+        Scope(state: \.tab2, action: \.tab2) {
+            CounterFeature()
+        }
+        Reduce { state, action in
+            
+            return .none
+        }
+    }
+}
+
+struct AppView: View {
+    // have isolated stores is not ideal if we want them to have the ability to communicate w/ each other
+    // we should compose features together and power them with a single store
+    let store1: StoreOf<CounterFeature>
+    let store2: StoreOf<CounterFeature>
+    var body: some View {
+        TabView {
+            CounterView(store: store1)
+                .tabItem {
+                    Text("Counter 1")
+                }
+            CounterView(store: store2)
+                .tabItem {
+                    Text("Counter 2")
+                }
+        }
+    }
+}
