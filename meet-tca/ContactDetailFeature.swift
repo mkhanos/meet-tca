66955298f1d83c9feb813f05f4f6234561fbb0bb
Momo Khan <mkhanongm@gmail.com>
2025-04-18 15:15:45 -0400

    Implement delete button actions and add delete button to view in contact detail

--- meet-tca/ContactDetailFeature.swift
+++ meet-tca/ContactDetailFeature.swift
@@ -13,25 +13,65 @@ struct ContactDetailFeature {
     @ObservableState
     struct State: Equatable {
         let contact: Contact
+        @Presents var alert: AlertState<Action.Alert>?
     }
     
-    enum Action {}
+    enum Action {
+        case alert(PresentationAction<Alert>)
+        case delegate(Delegate)
+        case deleteButtonTapped
+        enum Alert {
+            case confirmDeletion
+        }
+        enum Delegate {
+            case confirmDeletion
+        }
+    }
     
+    @Dependency(\.dismiss) var dismiss
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
-                
+            case .alert(.presented(.confirmDeletion)):
+                return .run { send in
+                    await send(.delegate(.confirmDeletion))
+                    await self.dismiss()
+                }
+            case .deleteButtonTapped:
+                state.alert = .confirmDeletion
+                return .none
+            case .delegate:
+                return .none
+            case .alert:
+                return .none
             }
         }
+        .ifLet(\.alert, action: \.alert)
     }
+}
+
+
+
+extension AlertState where Action == ContactDetailFeature.Action.Alert {
+  static let confirmDeletion = Self {
+    TextState("Are you sure?")
+  } actions: {
+    ButtonState(role: .destructive, action: .confirmDeletion) {
+      TextState("Delete")
+    }
+  }
 }
 
 struct ContactDetailView: View {
     let store: StoreOf<ContactDetailFeature>
     var body: some View {
         Form {
+            Button("Delete") {
+                store.send(.deleteButtonTapped)
+            }
         }
         .navigationTitle(Text(store.contact.name))
+        .alert($store.scope(state: \.alert, action: \.alert))
     }
 }
 
